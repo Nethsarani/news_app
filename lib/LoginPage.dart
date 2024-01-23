e85ca3887beaccf41054a7b5b95bdee4c801b963
Nethsarani <129503280+Nethsarani@users.noreply.github.com>
2024-01-23 20:48:04 +0530

    update lib/LoginPage.dart

--- lib/LoginPage.dart
+++ lib/LoginPage.dart
@@ -10,5 +10,57 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  
+  TextFormField (
+    decoration: const InputDecoration(
+      labelText: 'Username',
+      focusedBorder: OutlineInputBorder(
+        borderSide: BorderSide(
+          color: Colors.blue
+        ),
+      ),
+      enabledBorder: OutlineInputBorder(
+        borderSide: BorderSide(
+          color: Colors.grey
+        ),
+      ),
+    ),
+  ),
+  
+  TextFormField (
+    decoration: const InputDecoration(
+      labelText: 'Password',
+      focusedBorder: OutlineInputBorder(
+        borderSide: BorderSide(
+          color: Colors.blue
+        ),
+      ),
+      enabledBorder: OutlineInputBorder(
+        borderSide: BorderSide(
+          color: Colors.grey
+        ),
+      ),
+    ),
+  ), 
+  
+  ElevatedButton(
+    onPressed: () {
+      Navigator.push(
+        context,MaterialPageRoute(
+          builder: (context) => const LoginScreen()
+        ),
+      );
+    },
+    style: ElevatedButton.styleFrom(
+      backgroundColor: Colors.blue[600],
+      shape: BeveledRectangleBorder(
+        borderRadius: BorderRadius.circular(2),
+      ),
+    ),
+    child: const Text("Login",
+      style: TextStyle(color: Colors.white),
+    ),
+  ),  
+  
   }
 
